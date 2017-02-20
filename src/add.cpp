0c3eb12fb51cbbd90706dffbceee6a16ea582742
Красикова Екатерина Андреевна <krasikova.e@ccam.cmc.unn.net>
2017-02-20 18:30:01 +0300

    New function has been added

--- src/add.cpp
+++ src/add.cpp
@@ -1,3 +1,6 @@
 int add(int x, int y) {
   return x + y;
 }
+int mult(int x, int y) {
+	return x*y;
+}
--- test/test.cpp
+++ test/test.cpp
@@ -5,7 +5,9 @@ TEST(Addition, CanAddTwoNumbers) {
   EXPECT_EQ(add(2, 2), 4);
   EXPECT_EQ(add(-2, 2), 0);
 }
-
+TEST(Mult, CanMultTwoNumbers) {
+	EXPECT_EQ(mult(2, 3), 6);
+}
 int main(int ac, char* av[]) {
   testing::InitGoogleTest(&ac, av);
   return RUN_ALL_TESTS();
