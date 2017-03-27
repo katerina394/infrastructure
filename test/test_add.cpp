f470bdb1267fb2c45c220ab85fe561e12bdf4b0b
Красикова Екатерина Андреевна <krasikova.e@ccam.cmc.unn.net>
2017-03-27 19:33:17 +0300

    tabs was deleted2

--- test/test_add.cpp
+++ test/test_add.cpp
@@ -2,11 +2,11 @@
 #include "add.h"
 
 TEST(ways, can_find_way) {
-	CNode a;
-	a.x = 2;
-	a.y = 1;
-	char *res = ways(a);
-	char *exp = "EWESN";
-	for (int i = 0; i < 5; i++)
-		EXPECT_EQ(res[i], exp[i]);
+    CNode a;
+    a.x = 2;
+    a.y = 1;
+    char *res = ways(a);
+    char *exp = "EWESN";
+    for (int i = 0; i < 5; i++)
+        EXPECT_EQ(res[i], exp[i]);
 }
