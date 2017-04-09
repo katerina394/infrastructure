d634281bb1e1f4a5d97b014df7cfefb10ac21a21
katerina394 <katrinkrasikova@mail.ru>
2017-04-09 22:33:45 +0300

    Class RBTree was added

---
+++ include/RBTree.h
@@ -0,0 +1,245 @@
+#ifndef INCLUDE_RBTREE_H_
+#define INCLUDE_RBTREE_H_
+#include "rbnode.h"
+template <class T>
+class RBTree {
+    rbnode<T>* root;
+    void RotateLeft(rbnode<T> *x);
+    void RotateRight(rbnode<T> *x);
+    void InsertFixup(rbnode<T> *x);
+    void DeleteFixup(rbnode<T> *x);
+public:
+    RBTree(rbnode<T> rt = NIL);
+    ~RBTree(void);
+    RBTree Insert(T v);
+    RBTree Delete(T v);
+    rbnode<T>* Find(T v);
+};
+
+template <class T>
+RBTree<T>::RBTree(rbnode<T> rt) {
+    root = rt;
+}
+template <class T>
+RBTree<T>::~RBTree() {
+}
+template <class T>
+void RBTree<T>::RotateLeft(rbnode<T> *x) {
+    rbnode<T> *y = x->right;
+    x->right = y->left;
+    if (y->left != NIL)
+        y->left->parent = x;
+    y->parent = x->parent;
+    if (x->parent != 0) {
+        if (x == x->parent->left) {
+            x->parent->left = y;
+        } else {
+            x->parent->right = y;
+        }
+    } else {
+        root = y;
+    }
+    y->left = x;
+    x->parent = y;
+}
+template <class T>
+void RBTree<T>::RotateRight(rbnode<T> *x) {
+    y = x->left;
+    x->left = y->right;
+    if (y->right != NIL)
+        y->right->parent = x;
+    y->parent = x->parent;
+    if (x.parent != 0) {
+        if (x = x->parent->left) {
+            x->parent->left = y;
+        } else {
+            x->parent->right = y;
+        }
+    } else {
+        root = y;
+    }
+    y->right = x;
+    x->parent = y;
+}
+template <class T>
+void RBTree<T>::InsertFixup(rbnode<T> *x) {
+    while ( (x != root) && (x->parent->color == 1) ) {
+        if (x->parent == x->parent->parent->left) {
+            rbnode<T> *y = x->parent->parent->right;
+            if (y->color == 1) {
+                x->parent->color = 0;
+                y->color = 0;
+                x->parent->parent->color = 1;
+                x = x->parent->parent;
+            } else {
+                if (x == x->parent->right) {
+                    x = x->parent;
+                    RotateLeft(x);
+                }
+                x->parent->color = 0;
+                x->parent->parent->color = 1;
+                RotateRight(x->parent->parent);
+            }
+        } else {
+            rbnode<T> *y = x->parent->parent->left;
+            if (y->color == 1) {
+                x->parent->color = 0;
+                y->color = 0;
+                x->parent->parent->color = 1;
+                x = x->parent->parent;
+            } else {
+                if (x == x->parent->left) {
+                    x = x->parent;
+                    RotateRight(x);
+                }
+                x->parent->color = 0;
+                x->parent->parent->color = 1;
+                RotateLeft(x->parent->parent);
+            }
+        }
+    }
+    root->color = 0;
+}
+template <class T>
+RBTree RBTree<T>::Insert(T v) {
+    rbnode<T> *tmp = root, *ptmp = 0, *x = 0;
+    while (tmp != NIL) {
+        if (tmp->val == v) throw 1;
+        ptmp = tmp;
+        if (v < tmp->val) {
+            tmp = tmp->left;
+        } else {
+            tmp = tmp->right;
+        }
+    }
+    x = new rbnode<T>;
+    x->parent = ptmp;
+    x->left = x->right = NIL;
+    x->color = 1;    
+    x->val = v;
+    if (ptmp != 0) {
+        if ( v < ptmp->val) {
+            ptmp->left = x;
+        } else {
+            ptmp->right = x;
+        }
+    } else {
+        root = x;
+    }
+    InsertFixup(x);
+    return *this;
+}
+template <class T>
+void RBTree<T>::DeleteFixup(rbnode<T> *x) {
+    while ( (x != root) && (x->color == 0) ) {
+        if (x == x->parent->left) {
+            rbnode<T> *y = x->parent->right;
+            if (y->color == 1) {
+                y->color = 0;
+                x->parent->color = 1;
+                RotateLeft(x->parent);
+                y = x->parent->right;
+            }
+            if ( (y->left->color == 0) && (y->right->color == 0) ) {
+                y->color = 1;
+                x = x->parent;
+            } else {
+                if (y->right->color == 0) {
+                    y->left->color = 0;
+                    y->color = 1;
+                    RotateRight(y);
+                    y = x->parent->right;
+                }
+                y->color = x->parent->color;
+                x->parent->color = 0;
+                y->right->color = 0;
+                RotateLeft(x->parent);
+                x = root;
+            }
+        } else {
+            rbnode<T> *y = x->parent->left;
+            if (y->color == 1) {
+                y->color = 0;
+                x->parent->color = 1;
+                RotateRight(x->parent);
+                y = x->parent->left;
+            }
+            if ( (y->right->color == 0) && (y->left->color == 0) ) {
+                y->color = 1;
+                x = x->parent;
+            } else {
+                if (y->left->color == 0) {
+                    y->right->color = 0;
+                    y->color = 1;
+                    RotateLeft(y);
+                    y = x->parent->left;
+                }
+                y->color = x->parent->color;
+                x->parent->color = 0;
+                y->left->color = 0;
+                RotateRight(x->parent);
+                x = root;
+            }
+        }
+    }
+    x->color = 0;
+}
+template <class T>
+RBTree RBTree<T>::Delete(T v) {
+    rbnode<T> *x, *y, *tmp;
+    tmp = root;
+    while (tmp != NIL) {
+        if (z->val == v) {
+            break;
+        } else {
+            if (v < z->val) {
+                z = z->left;
+            } else {
+                z = z->right;
+            }
+        }
+    }
+    if (z == NIL) throw 1;
+    if ( (z->left == NIL) || (z->right == NIL) ) {
+        y = z;
+    } else {
+        y = z->right;
+        while (y->left != NIL) y = y->left;
+    }
+    if (y->left != NIL) {
+        x = y->left;
+    } else {
+        x = y->right;
+    }
+    x->parent = y->parent;
+    if (y->parent != 0) {
+        if (y == y->parent->left) {
+            y->parent->left = x;
+        } else {
+            y->parent->right = x;
+        }
+    } else {
+        root = x;
+    }
+    if (y != z) z->val = y->val;
+    if (y->color == 0) DeleteFixup(x);
+    delete y;
+    return *this;
+}
+template <class T>
+rbnode<T>* RBTree<T>::Find(T v) {
+    rbnode<T> *tmp = root;
+    while (tmp != 0) {
+        if (v == tmp->val) {
+            return tmp;
+        } else {
+            if (v < tmp->val) {
+                tmp = tmp->left;
+            } else {
+                tmp = tmp->right;
+            }    
+        }
+    }
+    throw 1;
+}
+#endif  // INCLUDE_RBTREE_H_
---
+++ include/rbnode.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDE_RBNODE_H_
+#define INCLUDE_RBNODE_H_
+template <class T>
+struct rbnode {
+    T val;
+    rbnode* left, right, parent;
+    int color;
+}
+rbnode<int> NILnode = {0, 0, 0, 0, 0};
+#define NIL &NILnode
+#endif  // INCLUDE_RBNODE_H_
