d5566c0b24e9fb7046db13bc4c67f35b96a162c4
katerina394 <katrinkrasikova@mail.ru>
2017-04-30 17:11:40 +0300

    PercolationWidth was changed

--- src/islands.cpp
+++ src/islands.cpp
@@ -1,10 +1,7 @@
 #include <stdlib.h>
 #include <vector>
 #include <queue>
-struct index {
-    int str;
-    int stlb;
-};
+#include "islands.h"
 int Find(std::vector<int> a, int v) {
     int tmp = v;
     while (a[tmp] != tmp)
@@ -15,8 +12,7 @@ void Union(std::vector<int> *a, std::vector<int> *b, int v1, int v2) {
     if ((*b)[Find(*a, v1)] > (*b)[Find(*a, v2)]) {
         (*a)[Find(*a, v2)] = (*a)[Find(*a, v1)];
         (*b)[Find(*a, v1)]++;
-    }
-    else {
+    } else {
         (*a)[Find(*a, v1)] = (*a)[Find(*a, v2)];
         (*b)[Find(*a, v2)]++;
     }
@@ -63,22 +59,20 @@ std::vector<std::vector<int>> PercolationUF(std::vector<std::vector<int>> m) {
             if (Find(c, i*mSize + j + 1) != Find(c, 0)) m[i][j] = 0;
     return m;
 }
-std::vector<std::vector<int>> PercolationWidth(std::vector<std::vector<int>> m) {
-    int mSize = m.size();
-    std::vector<std::vector<int>> b(mSize, std::vector<int>(mSize, 1000));
-    std::queue<index> q;
-    index y;
+void BFS(std::vector<std::vector<int>> m, std::vector<std::vector<int>> &b,
+    index y) {
     index* temp;
-    y.str = 0;
-    y.stlb = mSize - 1;
+    int mSize = m.size();
+    std::queue<index> q;
     q.push(y);
-    b[y.str][y.stlb] = 0;
+    b[y.str][y.stlb] = 1;
     while (!q.empty()) {
         index tmp = q.front();
         q.pop();
         if (tmp.stlb - 1 > -1) {
-            if ((m[tmp.str][tmp.stlb - 1] == 1) && (b[tmp.str][tmp.stlb - 1] == 1000)) {
-                b[tmp.str][tmp.stlb - 1] = b[tmp.str][tmp.stlb] + 1;
+            if ((m[tmp.str][tmp.stlb - 1] == 1) &&
+                (b[tmp.str][tmp.stlb - 1] == 1000)) {
+                b[tmp.str][tmp.stlb - 1] = 1;
                 temp = new index;
                 temp->str = tmp.str;
                 temp->stlb = tmp.stlb - 1;
@@ -86,8 +80,9 @@ std::vector<std::vector<int>> PercolationWidth(std::vector<std::vector<int>> m) 
             }
         }
         if (tmp.str - 1 > -1) {
-            if ((m[tmp.str - 1][tmp.stlb] == 1) && (b[tmp.str - 1][tmp.stlb] == 1000)) {
-                b[tmp.str - 1][tmp.stlb] = b[tmp.str][tmp.stlb] + 1;
+            if ((m[tmp.str - 1][tmp.stlb] == 1) &&
+                (b[tmp.str - 1][tmp.stlb] == 1000)) {
+                b[tmp.str - 1][tmp.stlb] = 1;
                 temp = new index;
                 temp->str = tmp.str - 1;
                 temp->stlb = tmp.stlb;
@@ -95,8 +90,9 @@ std::vector<std::vector<int>> PercolationWidth(std::vector<std::vector<int>> m) 
             }
         }
         if (tmp.stlb + 1 < mSize) {
-            if ((m[tmp.str][tmp.stlb + 1] == 1) && (b[tmp.str][tmp.stlb + 1] == 1000)) {
-                b[tmp.str][tmp.stlb + 1] = b[tmp.str][tmp.stlb] + 1;
+            if ((m[tmp.str][tmp.stlb + 1] == 1) &&
+                (b[tmp.str][tmp.stlb + 1] == 1000)) {
+                b[tmp.str][tmp.stlb + 1] = 1;
                 temp = new index;
                 temp->str = tmp.str;
                 temp->stlb = tmp.stlb + 1;
@@ -104,8 +100,9 @@ std::vector<std::vector<int>> PercolationWidth(std::vector<std::vector<int>> m) 
             }
         }
         if (tmp.str + 1 < mSize) {
-            if ((m[tmp.str + 1][tmp.stlb] == 1) && (b[tmp.str + 1][tmp.stlb] == 1000)) {
-                b[tmp.str + 1][tmp.stlb] = b[tmp.str][tmp.stlb] + 1;
+            if ((m[tmp.str + 1][tmp.stlb] == 1) &&
+                (b[tmp.str + 1][tmp.stlb] == 1000)) {
+                b[tmp.str + 1][tmp.stlb] = 1;
                 temp = new index;
                 temp->str = tmp.str + 1;
                 temp->stlb = tmp.stlb;
@@ -113,8 +110,30 @@ std::vector<std::vector<int>> PercolationWidth(std::vector<std::vector<int>> m) 
             }
         }
     }
-    for (int i = 0; i < mSize; ++i)
-        for (int j = 0; j < mSize; ++j)
+}
+std::vector<std::vector<int>> PercolationWidth(std::vector<std::vector<int>>
+    m) {
+    int mSize = m.size();
+    std::vector<std::vector<int>> b(mSize, std::vector<int>(mSize, 1000));
+    index y;
+    y.str = 0;
+    for (y.stlb = 0; y.stlb < mSize; ++y.stlb) {
+        if (m[y.str][y.stlb] == 1) BFS(m, b, y);
+    }
+    y.str = mSize-1;
+    for (y.stlb = 0; y.stlb < mSize; ++y.stlb) {
+        if (m[y.str][y.stlb] == 1) BFS(m, b, y);
+    }
+    y.stlb = 0;
+    for (y.str = 0; y.str < mSize; ++y.str) {
+        if (m[y.str][y.stlb] == 1) BFS(m, b, y);
+    }
+    y.stlb = mSize-1;
+    for (y.str = 0; y.str < mSize; ++y.str) {
+        if (m[y.str][y.stlb] == 1) BFS(m, b, y);
+    }
+    for (int i = 1; i < mSize-1; ++i)
+        for (int j = 1; j < mSize-1; ++j)
             if (b[i][j] == 1000) m[i][j] = 0;
     return m;
 }
