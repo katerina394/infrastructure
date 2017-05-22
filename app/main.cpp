77f4408f53357dc4bb53fbb49fb1a4bad26bd5da
katerina394 <katrinkrasikova@mail.ru>
2017-05-22 22:19:35 +0300

    main.cpp was added

--- app/main.cpp
+++ app/main.cpp
@@ -1,4 +1,19 @@
+#include <Dijkstra.h>
+#include <rbnode.h>
 #include <iostream>
-
-int main() {
+#include <vector>
+#include <utility>
+#define pair std::pair
+#define vector std::vector
+rbnode<int> NILnode;
+int main(int argc, char *argv[]) {
+    vector <int> res;
+    vector < vector < pair<int, int> > > gr = CreateGraph(argv[1]);
+    if (*(argv[2]) == '1') {
+        res = DijkstraRBTree(gr, atoi(argv[3]));
+    } else {
+        res = DijkstraFourHeap(gr, atoi(argv[3]));
+    }
+    for (int i = 0; i < static_cast<int>(res.size()); ++i)
+        std::cout << res[i] << ' ';
 }
